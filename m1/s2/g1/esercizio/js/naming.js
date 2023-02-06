5bc2170cdd272a4ff9bc601d6af318682abeafce
Perryno <you@example.com>
2023-02-06 17:45:03 +0100

    esercizio

---
+++ m1/s2/g1/esercizio/js/naming.js
@@ -0,0 +1,49 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+var petPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+console.log(_pet2);
+
+document.getElementById('ciao').textContent ='Non ti saluto più';
+
+
+ 
+
+var uno = prompt ('primo valore da sommare') 
+var due = prompt ('secondo valore da sommare')
+
+
+
+   var risultato = console.log(uno*due)
+   document.write(parseInt(uno) + parseInt (due))
+
+
+var myArray = ['pantera', 'leone', 'giraffa']; //preso da internet questo meccanismo random
+var rand = Math.random()*myArray.length | 0;
+var rValue = myArray[rand];
+var parola = console.log(rValue)
+
+var lunghezza = rValue.length //fatto io
+console.log(lunghezza)
+
+
+if (lunghezza > 5) {
+    document.write(' la ' + myArray[rand])
+     //non riesco a far scrivere in html il contenuto della variabile parola ps. ho smattato ma ho risolto
+    
+}
+else {
+    document.write (' il ' + myArray[rand])
+}
+
+document.write ('.  Rimane il fatto che il mio animale preferito é il '+ petPreferito +'.')
+//var 12 = 'numero';
